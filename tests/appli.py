45552a0ca6e369e5b18c4481bc7d811bc90566d8
Joseph Mirabel <jmirabel@laas.fr>
2019-05-24 17:27:06 +0200

    Fix SoT test

--- tests/appli.py
+++ tests/appli.py
@@ -2,16 +2,10 @@ from dynamic_graph.sot.core.meta_tasks_kine import MetaTaskKine6d, MetaTaskKineC
 from dynamic_graph.sot.core.matrix_util import matrixToTuple
 from numpy import eye
 
-taskRH    = MetaTaskKine6d('rh',robot.dynamic,'rh',robot.OperationalPointsMap['right-wrist'])
+taskRH    = MetaTaskKine6d('rh',robot.dynamic,'rh',robot.OperationalPointsMap['wrist'])
 handMgrip = eye(4); handMgrip[0:3,3] = (0.1,0,0)
 taskRH.opmodif = matrixToTuple(handMgrip)
 taskRH.feature.frame('desired')
-
-# --- STATIC COM (if not walking)
-#taskCom = MetaTaskKineCom(robot.dynamic)
-#robot.dynamic.com.recompute(0)
-#taskCom.featureDes.errorIN.value = robot.dynamic.com.value
-#taskCom.task.controlGain.value = 10
 
 from dynamic_graph import plug
 from dynamic_graph.sot.core import SOT
@@ -26,5 +20,4 @@ from dynamic_graph import plug
 plug (robot.device.state, ros_publish_state.state)
 robot.device.after.addDownsampledSignal ("ros_publish_state.trigger", 100)
 
-sot.push(taskCom.task.name)
 robot.device.control.recompute(0)
