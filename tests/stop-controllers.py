b4a42582939d8fda077c98a67c44d7f017473ef3
Joseph Mirabel <jmirabel@laas.fr>
2019-08-06 18:25:47 +0200

    Update stop-controllers + sot_keep scripts.

--- tests/stop-controllers.py
+++ tests/stop-controllers.py
@@ -5,5 +5,5 @@ from controller_manager_msgs.srv import SwitchController
 
 rospy.init_node('StoppingControllers')
 swCtl=rospy.ServiceProxy('/controller_manager/switch_controller',SwitchController)
-resp = swCtl(stop_controllers=['arm_controller','head_controller','torso_controller','gripper_controller'],strictness=2)
+resp = swCtl(stop_controllers=['mobile_base_controller', 'arm_controller','head_controller','torso_controller','gripper_controller'],strictness=2)
 
