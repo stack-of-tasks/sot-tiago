ebcb3fd9ee2a0bbd6d7e2c01108733898908b038
Joseph Mirabel <jmirabel@laas.fr>
2020-06-11 18:55:38 +0200

    Switch to Boost Python

---
+++ src/device-python.hh
@@ -0,0 +1,5 @@
+#include "sot-tiago-device.hh"
+
+namespace dg = dynamicgraph;
+
+typedef boost::mpl::vector< SoTTiagoDevice > entities_t;
---
+++ src/diff_drive_controller-python-module.cc
@@ -0,0 +1,29 @@
+#include <pinocchio/fwd.hpp>
+#include "dynamic-graph/python/module.hh"
+
+#include "diff_drive_controller.h"
+#include "holonomic-projection.h"
+#include "odometry.h"
+#include "speed_limiter.h"
+
+namespace dg = dynamicgraph;
+
+typedef boost::mpl::vector<
+  dg::DiffDriveController
+, dg::HolonomicProjection
+, dg::Odometry
+> entities_t;
+
+struct register_entity
+{
+template<typename T> inline void operator()(boost::type<T>) const
+{
+  dynamicgraph::python::exposeEntity<T>();
+}
+};
+
+BOOST_PYTHON_MODULE(wrap)
+{
+  bp::import("dynamic_graph");
+  boost::mpl::for_each<entities_t, boost::type<boost::mpl::_> >(register_entity());
+}
